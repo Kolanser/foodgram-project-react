ae2f20509b2cf1dc5dac4a11611caf45609365bc
Николай Слесарев <kolansers@yandex.ru>
2022-12-13 14:37:25 +0300

    Реализована подписка, отписка и представление подписок по документации

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import (
-    FollowViewSet, IngredientViewSet, RecipeViewSet, TagViewSet, CustomUserViewSet
+    IngredientViewSet, RecipeViewSet, TagViewSet, CustomUserViewSet
 )
 
 
@@ -9,11 +9,8 @@ router = routers.DefaultRouter()
 router.register('ingredients', IngredientViewSet, basename='ingredient')
 router.register('tags', TagViewSet, basename='tag')
 router.register('recipes', RecipeViewSet, basename='recipe')
-# router.register('users', FollowViewSet, basename='subscription')
 router.register('users', CustomUserViewSet, basename='user')
 urlpatterns = [
-    # path('', include('djoser.urls')),
-    # path('', CustomUserViewSet),
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -9,6 +9,10 @@ from .serializers import (
     TagSerializer,
     RecipeWriteSerializer
 )
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from djoser.views import UserViewSet
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
@@ -35,48 +39,11 @@ class RecipeViewSet(ModelViewSet):
             return RecipeWriteSerializer
         return RecipeSerializer
 
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import action
-
-
-class FollowViewSet(ModelViewSet):
-    """Получение подписок."""
-    serializer_class = FollowSerializer
-    queryset = Follow.objects.all()
-
-    # def perform_create(self, serializer):
-    #     user_id = self.kwargs.get('user_id')
-    #     following = get_object_or_404(CustomUser, id=user_id)
-    #     serializer.save(user=self.request.user, following=following)
-
-    # @action(methods=['post', 'delete'], detail=True)
-    # def subscribe(self, request, pk=None):
-    #     following = get_object_or_404(CustomUser, id=pk)
-    #     user = request.user
-    #     if (self.request.method == 'DELETE' and
-    #             user.subscriptions.filter(following=following)):
-    #         Follow.objects.filter(
-    #             user=request.user, following=following
-    #         ).delete()
-    #         return Response(status=status.HTTP_204_NO_CONTENT)
-    #     elif (self.request.method == 'POST' and
-    #             not user.subscriptions.filter(following=following)):
-    #         Follow.objects.create(user=request.user, following=following)
-    #         return Response(status=status.HTTP_200_OK)
-    #     return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    # @action(detail=False, )
-    # def subscriptions(self, request, pk=None):
-    #     pass
-
-
-from djoser.views import UserViewSet
 
 class CustomUserViewSet(UserViewSet):
     @action(methods=['post', 'delete'], detail=True)
-    def subscribe(self, request, pk=None):
-        following = get_object_or_404(CustomUser, id=pk)
+    def subscribe(self, request, id=None):
+        following = get_object_or_404(CustomUser, id=id)
         user = request.user
         if (self.request.method == 'DELETE' and
                 user.subscriptions.filter(following=following)):
@@ -91,21 +58,13 @@ class CustomUserViewSet(UserViewSet):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False)
-    def subscriptions(self, request, pk=None):
-        # instance = self.get_object()
-        instance = get_object_or_404(Follow, user=request.user)
-        # serializer_class_obj = FollowSerializer
-        serializer_class_obj = self.get_serializer(instance=instance)
-        # serializer_class_obj = FollowSerializer(instance=instance)
+    def subscriptions(self, request):
+        serializer_class_obj = self.get_serializer(
+            request.user.subscriptions, many=True
+        )
         return Response(serializer_class_obj.data)
-
-    # @action(detail=True)
-    # def interfaces(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     serializer_obj = self.get_serializer(instance=instance)
-    #     return Response(serializer_obj.data)
 
     def get_serializer_class(self):
         if self.action == 'subscriptions':
             return FollowSerializer
-        return self.serializer_class
+        return super().get_serializer_class()
